d13ed518d99067b17b31e30624c0b7e3373b7b0a
Eric Hafvenstein <edhavey@null.net>
2021-10-15 16:29:13 -0500

    assignment complete

--- src/index.js
+++ src/index.js
@@ -3,19 +3,27 @@ const JSON_SERVER_API_ENDPOINT = "http://localhost:3000/toys/";
 const addToyForm = document.querySelector(".add-toy-form");
 const createToyBtn = document.querySelector(".submit");
 
+let addToy = false;
+
 class Toy {
-  constructor(name = "Jessie",
-   image = "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
-   likes = 0
-   ) {
+  constructor(name, image, likes = 0) {
     this.name = name;
     this.image = image;
     this.likes = likes;
   }
 }
 
-let addToy = false;
-
+const addLike = (card, id) => {
+  const likes = ++card.querySelector(".likes").textContent; // I'm very proud of this line of code
+  fetch(JSON_SERVER_API_ENDPOINT + id, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({ likes })    
+  })
+};
 
 const displayCards = (cards) => {
   const toyCollection = document.querySelector("#toy-collection");
@@ -24,31 +32,31 @@ const displayCards = (cards) => {
 } 
 
 const buildCards = toyData => {
-  // const toyCollection = document.querySelector("#toy-collection");
   const builtCards = [];
   toyData.forEach((_toy, i, toyData) => {
-    const {name, image, id} = toyData[i];
+    const {name, image, id, likes} = toyData[i];
     const newCard = document.createElement('DIV');
     const newCardHTML = `
       <h2>${name}</h2>
       <img src="${image}" class="toy-avatar" />
-      <p>4 Likes </p>
-      <button class="like-btn" id="${id}">Like <3</button>
+      <p><span class="likes">${likes}</span> Likes </p>
+      <button class="like-btn" id="${id}" onclick="addLike(this.parentElement, this.id)">Like <3</button>
     `;
-    newCard.classList.add("card");
     newCard.innerHTML = newCardHTML;
+    newCard.classList.add("card");
+    // newCard.querySelector('button').addEventListener('click', (e) => {
+    //   console.log(e, e.target, this);
+    // });
     builtCards.unshift(newCard.outerHTML);
   })
   displayCards(builtCards)
 }
 
-const fetchToys = () => {
+const getToys = () => {
   fetch(JSON_SERVER_API_ENDPOINT)
     .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      return buildCards(data)
-    }).catch(err => console.error(err))
+    .then(data => buildCards(data))
+    .catch(err => console.error(err))
     
 }
 
@@ -65,19 +73,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
   addToyForm.addEventListener("submit", e => {
-    debugger;
     e.preventDefault();
+    const formInput = [...document.querySelectorAll('input[type="text"]')];
+    const [{value: name}, {value: image}] = formInput;
     fetch(JSON_SERVER_API_ENDPOINT, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "Accept": "application/json"
       },
-      body: JSON.stringify(new Toy())
-    }).then(() => fetchToys())
+      body: JSON.stringify(new Toy(name, image))
+    }).then(() => getToys())
     .catch(err => console.error(err));
   });
-  fetchToys();
+  getToys();
 });
 
 
